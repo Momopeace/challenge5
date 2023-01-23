87bc5ca8b1648652d37a8db63be9997a226722e2
momopeace <[mohamedkadourparis@gmail.com*](mailto:mohamedkadourparis@gmail.com)>
2023-01-23 16:46:47 +0100

    challenge finis

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { User } from '../user';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+  model: User = new User("","","","");
+
+  constructor(){}
+  onSubmit(): void {
+    // Formulaire envoyé
+    console.log(this.model);
+  }
+
+}
